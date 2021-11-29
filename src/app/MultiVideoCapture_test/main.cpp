102b12c04a524589c25578085d113986841dcd1e
Jaemyun Kim <jaemyunkim@outlook.com>
2021-11-29 20:28:37 +0900

    update the test driver

--- src/app/MultiVideoCapture_test/main.cpp
+++ src/app/MultiVideoCapture_test/main.cpp
@@ -10,14 +10,15 @@ int main() {
 	std::vector<int> camIds = { 0, 1 };
 	std::vector<FrameType> images(camIds.size());
 	//cv::Size resolution = { 1920, 1080 };
-	cv::Size resolution = { 1280, 720 };
+	//cv::Size resolution = { 1280, 720 };
 	//cv::Size resolution = { 800, 600 };
 	//cv::Size resolution = { 640, 480 };
-	//cv::Size resolution = { 640, 360 };
+	cv::Size resolution = { 640, 360 };
+	//cv::Size resolution = { 320, 240 };
 	float fps = 30.f;
 
-	MultiVideoCapture mvc;
-	mvc.open(camIds, CV_CAP_DSHOW);
+	MultiVideoCapture mvc(true);
+	mvc.open(camIds, CV_CAP_DSHOW, true);
 	mvc.set(camIds, resolution, fps);
 
 	std::chrono::milliseconds duration(long(1000.f / fps));
@@ -35,7 +36,7 @@ int main() {
 		for (int i = 0; i < camIds.size(); i++) {
 			cv::flip(images[i].mat(), images[i].mat(), 1);	// horizontal flip
 
-			int id = camIds[i];
+			const int id = camIds[i];
 			cam_times[i] = images[i].timestamp();
 			if (!images[i].empty())
 				cv::imshow("cam " + std::to_string(id), images[i].mat());
@@ -46,8 +47,8 @@ int main() {
 		std::chrono::duration<double> capture_sec = capture_times[1] - capture_times[0];
 		std::chrono::duration<double> diff_sec = cam_times[0] - cam_times[1];
 		diff_sec = (diff_sec >= diff_sec.zero()) ? diff_sec : -diff_sec;
-		std::cout << "capture time: " << std::setw(10) << std::left << capture_sec.count() << " sec";
-		std::cout << "\ttime difference: " << std::setw(10) << std::left << diff_sec.count() << " sec" << std::endl;
+		std::cout << "capture time: " << std::setw(9) << std::left << capture_sec.count() << " sec";
+		std::cout << "\ttime difference: " << std::setw(9) << std::left << diff_sec.count() << " sec" << std::endl;
 
 		c = cv::waitKey(1);
 
